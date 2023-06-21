61309efb7f4e32a33caf2a1535b17c7031d1532f
faye <oluwafayefunmiokude@gmail.com>
2023-06-21 02:09:41 +0100

    ich habe ein star

--- Day1/Day1.go
+++ Day1/Day1.go
@@ -5,7 +5,21 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func maxCalories(elf map[int]int) (int, int) {
+	max := -1
+	fatass_elf := -1
+	for elf_id, calories := range elf {
+		if calories > max {
+			max = calories
+			fatass_elf = elf_id
+		}
+	}
+	return fatass_elf, max
+}
 
 func main() {
 	file, err := os.Open("Input.txt")
@@ -15,28 +29,40 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
 	elf := make(map[int]int)
 	var total int
-	fmt.Println("Enter the elf food list")
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
 			if total != 0 {
 				elf[len(elf)+1] = total
+				total = 0
 			}
 			continue
 		}
 
+		num, err := strconv.Atoi(strings.TrimSpace(line))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		total += num
 	}
 
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// check if remaining total unaccounted for (last line, varies if there's a slash n at the end)
+	if total != 0 {
+		elf[len(elf)+1] = total
+	}
+
+	for id, calories := range elf {
+		fmt.Printf("Elf %d carrying food with %d calories\n", id, calories)
+	}
+
+	elf_id, calories_carried := maxCalories(elf)
+	fmt.Println("Elf", elf_id, "is carrying the most calories:", calories_carried)
 }
